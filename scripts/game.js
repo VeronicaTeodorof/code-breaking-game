b6b9b3ef24cff7485b3f5d10575898b299067ddd
VeronicaTeodorof <vteodorof@rocketmail.com>
2026-01-24 07:10:02 +0000

    Add code to generate a random 4 digit code

--- scripts/game.js
+++ scripts/game.js
@@ -2,14 +2,28 @@ function newGame() {
 
 }
 
+// Generates a 4 digit random code
 function generateCode() {
+    
+    let digit1 = Math.floor(Math.random() * 10);
+    let digit2 = Math.floor(Math.random() * 10);
+    let digit3 = Math.floor(Math.random() * 10);
+    let digit4 = Math.floor(Math.random() * 10);
 
+    let computerCode = [digit1, digit2, digit3, digit4];
+
+    console.log(computerCode);
 }
+
+
+
 
 function validateInput() {
 
 }
 
 function checkAnswer() {
-    
+
 }
+
+generateCode();
