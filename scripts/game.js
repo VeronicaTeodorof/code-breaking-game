e403e3e82b761eb7c28e55d8e74a806ecbef7df5
VeronicaTeodorof <vteodorof@rocketmail.com>
2026-01-24 07:23:03 +0000

    Add code to generate a random digit

--- scripts/game.js
+++ scripts/game.js
@@ -5,14 +5,19 @@ function newGame() {
 // Generates a 4 digit random code
 function generateCode() {
     
-    let digit1 = Math.floor(Math.random() * 10);
-    let digit2 = Math.floor(Math.random() * 10);
-    let digit3 = Math.floor(Math.random() * 10);
-    let digit4 = Math.floor(Math.random() * 10);
+    let digit1 = generateDigit();
+    let digit2 = generateDigit();
+    let digit3 = generateDigit();
+    let digit4 = generateDigit();
 
     let computerCode = [digit1, digit2, digit3, digit4];
 
     console.log(computerCode);
+}
+// Generates a random digit
+function generateDigit() {
+    let digit = Math.floor(Math.random() * 10);
+    return digit;
 }
 
 
