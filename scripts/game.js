714bb566573e7d274437ee1c0ee432d7d70b5982
VeronicaTeodorof <vteodorof@rocketmail.com>
2026-01-31 07:40:22 +0000

    Add DOMContentLoaded event listener

--- scripts/game.js
+++ scripts/game.js
@@ -1,58 +1,52 @@
-let playButton = document.getElementById("play-button");
-playButton.addEventListener("click", changeButton);
-playButton.addEventListener("click", enterCodeMessage);
-playButton.addEventListener("click", cursorReady);
+document.addEventListener("DOMContentLoaded", function() {
+    const playButton = document.getElementById("play-button");
 
+    // Add event listeners
+    playButton.addEventListener("click", changeButton);
+    playButton.addEventListener("click", enterCodeMessage);
+    playButton.addEventListener("click", cursorReady);
 
-// Change the text of the play button
-function changeButton() {
-    playButton.innerText = "Quit";
-}
-
-function enterCodeMessage() {
-    messageEnterCode = document.getElementById("message");
-    messageEnterCode.innerHTML = "<h2>Enter code here:</h2>";
-}
-
-// Sets the cursor ready for the user to type the first input
-function cursorReady() {
-    document.getElementById("input1").focus();
-}
-
-// Moves cursor to the next input after user enters a value
-function moveFocus() {
-    let inputBoxes = document.querySelectorAll(".input");
-    
-    for(let i = 0; i < inputBoxes.length - 1; i++) {
-        inputBoxes[i].addEventListener("input", () => inputBoxes[i+1].focus());
+    // Change the text of the play button
+    function changeButton() {
+        playButton.innerText = "Quit";
     }
-}
-moveFocus();
 
-// Generates a 4 digit random code:
-function generateCode() {
+    function enterCodeMessage() {
+        messageEnterCode = document.getElementById("message");
+        messageEnterCode.innerHTML = "<h2>Enter code here:</h2>";
+    }
+
+    // Sets the cursor ready for the user to type the first input
+    function cursorReady() {
+        document.getElementById("input1").focus();
+    }
+
+    // Moves cursor to the next input after user enters a value
+    function moveFocus() {
+        let inputBoxes = document.querySelectorAll(".input");
     
-    let digit1 = generateDigit();
-    let digit2 = generateDigit();
-    let digit3 = generateDigit();
-    let digit4 = generateDigit();
+        for(let i = 0; i < inputBoxes.length - 1; i++) {
+            inputBoxes[i].addEventListener("input", () => inputBoxes[i+1].focus());
+        }
+    }
+    moveFocus();
 
-    let computerCode = [digit1, digit2, digit3, digit4];
-    console.log(computerCode);
-};
+    // Generates a 4 digit random code:
+    function generateCode() {
+    
+        let digit1 = generateDigit();
+        let digit2 = generateDigit();
+        let digit3 = generateDigit();
+        let digit4 = generateDigit();
 
-generateCode();
-// Generates a random digit
-function generateDigit() {
-    let digit = Math.floor(Math.random() * 10);
-    return digit;
-}
+        let computerCode = [digit1, digit2, digit3, digit4];
+        console.log(computerCode);
+    };
 
-function checkAnswer() {
-
-   
-
-}
-
-checkAnswer();
-
+    generateCode();
+    // Generates a random digit
+    function generateDigit() {
+        let digit = Math.floor(Math.random() * 10);
+        return digit;
+    }
+});
